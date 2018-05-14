fb0d2f540ee00c9ba1f4367ed354409b40b5f6d3
Julie K <julieskim160@gmail.com>
2018-05-14 08:57:52 -0700

    sheepCounter

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+'use strict';
+
 //Counting Sheep
+function sheepCounter(sheep) {
+    if (sheep === 0) {
+        return "";
+    }
+    console.log(sheep + " - Another sheep jump over the fence")
+    sheepCounter(sheep - 1);
+}
+
+sheepCounter(3);
+
