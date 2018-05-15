bd33c851b22c9fc841125923a430e6c080e63c04
Julie K <julieskim160@gmail.com>
2018-05-14 19:17:14 -0700

    another solution for anagram

--- anagrams.js
+++ anagrams.js
@@ -1,17 +1,35 @@
-function anagram(prefix="", word) {
-    if(word.length <= 1) {
-        console.log(prefix + word);
-    } else {
-    for(var i = 0; i < word.length; i++) {
-        let firstLetter = word.substring(i, i+1);
-        let remainingLetters = word.substring(i+1);
-        let previousLetters = word.substring(0, i);
-        anagram(prefix+firstLetter, previousLetters+remainingLetters)
+function anagram(word, chunk){
+    if(word.length === 0){
+      console.log(chunk);
+      return;
     }
+    for(var i = 0; i < word.length; i++){
+      let prefix = word[i] + chunk;
+      let otherLetters = word.substr(0,i)+word.substr(i+1);
+      anagram(otherLetters, prefix);
+    }
+  }
+  
+  anagram("eas","");
 
-    }
-}
-console.log(anagram("", "east"))
+
+// function anagram(prefix="", word) {
+//     if(word.length <= 1) {
+//         console.log(prefix + word);
+//     } else {
+//     for(var i = 0; i < word.length; i++) {
+//         let firstLetter = word.substring(i, i+1);
+//         let remainingLetters = word.substring(i+1);
+//         let previousLetters = word.substring(0, i);
+//         anagram(prefix+firstLetter, previousLetters+remainingLetters)
+//     }
+
+//     }
+// }
+// console.log(anagram("", "east"))
+
+
+
 // function anagram(prefix="", word) {
 //     if(word.length === 1) {
 //         co[word];
